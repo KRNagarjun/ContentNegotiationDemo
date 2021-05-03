ff6dd21753341c0ced251e3e95afe6f2c754e560
KRNagarjun <kr.nagarjun@ltts.com>
2021-05-03 12:07:40 +0530

    Content negotiation demo

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentNegotiationDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [FormatFilter]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("{id}.{format?}")]
+        public Product Get(int id)
+        {
+            return new Product();
+        }
+    }
+
+    public class Product
+    {
+    }
+}
